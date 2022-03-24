d1b2164205d56dafd90fcf6ec4f5f47adb7512bd
Asharani <asharaniv.sangamone@gmail.com>
2022-03-24 15:45:25 +0530

    websecurity

---
+++ Web_Security1/src/main/java/com/sangamone/webSecurity/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sangamone.webSecurity;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebSecurity1Application.class);
+	}
+
+}
---
+++ Web_Security1/src/test/java/com/sangamone/webSecurity/WebSecurity1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangamone.webSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSecurity1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
